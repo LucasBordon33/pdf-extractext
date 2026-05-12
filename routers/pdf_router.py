4cf894a0376b7c2f79c95069003eb5ac17205f76
Helmy <51140357+Helmet-SpyCrab@users.noreply.github.com>
2026-05-12 10:59:50 -0300

    Update pdf_router.py
    Se agregó status_code=201 en POST, 204 en DELETE, y prefijo /api/v1

--- routers/pdf_router.py
+++ routers/pdf_router.py
@@ -1,21 +1,25 @@
-from fastapi import APIRouter
-from controllers import pdf_controller # Importamos el controlador que creamos
+from fastapi import APIRouter, status
+from controllers import pdf_controller
 from models.pdf import PDF
 
-router = APIRouter()
+router = APIRouter(prefix="/api/v1", tags=["crud"])
 
-@router.post("/pdfs")
+
+@router.post("/pdfs", status_code=status.HTTP_201_CREATED)
 def create(pdf: PDF):
     return pdf_controller.create_new_pdf(pdf)
+
 
 @router.get("/pdfs")
 def read():
     return pdf_controller.get_all_pdfs()
 
+
 @router.put("/pdfs/{pdf_id}")
 def update(pdf_id: str, pdf: PDF):
     return pdf_controller.update_existing_pdf(pdf_id, pdf)
 
-@router.delete("/pdfs/{pdf_id}")
+
+@router.delete("/pdfs/{pdf_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete(pdf_id: str):
-    return pdf_controller.delete_existing_pdf(pdf_id)
+    pdf_controller.delete_existing_pdf(pdf_id)
