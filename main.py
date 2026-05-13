62af33a6c9776d34198b177aac140d70c54b92bb
LucasBordon33 <lucasnayaresc7@gmail.com>
2026-05-13 17:11:02 -0300

    Cambios ligeros, docker_compose (para levantar MongoDB de forma local) descomentado

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ app = FastAPI(title="PDF Extractext API", version="1.0.0")
 pdf_router = PDFRouter()
 app.include_router(pdf_router.router)
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+#if __name__ == "__main__":
+ #   import uvicorn
+  #  uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
